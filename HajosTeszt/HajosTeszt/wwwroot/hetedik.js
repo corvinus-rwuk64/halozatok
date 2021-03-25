5dc27a3ce61d188403d6b762996f9eb90ac52046
Máté Plank <mate.plank@stud.uni-corvinus.hu>
2021-03-25 13:30:31 +0100

    JSON

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,72 @@
+﻿var kérdések;
+var léptetés = 0;
+window.onload(letöltés());
+function letöltés() {
+    fetch('/questions.json')
+        .then(r => r.json())
+        .then(d => letöltésBefejeződött(d)
+        );
+}
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    kérdések = d;
+    kérdésMegjelenítés(0);
+}
+function kérdésMegjelenítés(kérdés) {
+
+    document.getElementById("kérdés_szöveg").innerHTML = kérdések[kérdés].questionText;
+
+    document.getElementById("válasz1").innerHTML = kérdések[kérdés].answer1;
+    document.getElementById("válasz1").classList.add("rossz");
+    document.getElementById("válasz2").innerHTML = kérdések[kérdés].answer2;
+    document.getElementById("válasz2").classList.add("rossz");
+    document.getElementById("válasz3").innerHTML = kérdések[kérdés].answer3;
+    document.getElementById("válasz3").classList.add("rossz");
+
+    document.getElementById("válasz" + kérdések[kérdés].correctAnswer).classList.add("jó");
+    document.getElementById("válasz" + kérdések[kérdés].correctAnswer).classList.remove("rossz");
+
+    document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdés].image;
+}
+function következőKérdésMegjelenítés() {
+
+    léptetés++;
+    if (léptetés == 3) { léptetés = 0 }
+    kérdés = léptetés;
+    
+    document.getElementById("kérdés_szöveg").innerHTML = kérdések[kérdés].questionText;
+
+    document.getElementById("válasz1").innerHTML = kérdések[kérdés].answer1;
+    document.getElementById("válasz1").classList.add("rossz");
+    document.getElementById("válasz2").innerHTML = kérdések[kérdés].answer2;
+    document.getElementById("válasz2").classList.add("rossz");
+    document.getElementById("válasz3").innerHTML = kérdések[kérdés].answer3;
+    document.getElementById("válasz3").classList.add("rossz");
+
+    document.getElementById("válasz" + kérdések[kérdés].correctAnswer).classList.add("jó");
+    document.getElementById("válasz" + kérdések[kérdés].correctAnswer).classList.remove("rossz");
+
+    document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdés].image;
+}
+function előzőKérdésMegjelenítés() {
+
+    léptetés--;
+    if (léptetés == -1) { léptetés = 2 }
+    kérdés = léptetés;
+
+    document.getElementById("kérdés_szöveg").innerHTML = kérdések[kérdés].questionText;
+
+    document.getElementById("válasz1").innerHTML = kérdések[kérdés].answer1;
+    document.getElementById("válasz1").classList.add("rossz");
+    document.getElementById("válasz2").innerHTML = kérdések[kérdés].answer2;
+    document.getElementById("válasz2").classList.add("rossz");
+    document.getElementById("válasz3").innerHTML = kérdések[kérdés].answer3;
+    document.getElementById("válasz3").classList.add("rossz");
+
+    document.getElementById("válasz" + kérdések[kérdés].correctAnswer).classList.add("jó");
+    document.getElementById("válasz" + kérdések[kérdés].correctAnswer).classList.remove("rossz");
+
+    document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdés].image;
+}
+

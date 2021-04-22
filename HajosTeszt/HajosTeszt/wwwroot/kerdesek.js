021e190ad6dcaf317ac63b5b72b1da0a8d46736a
Máté Plank <mate.plank@stud.uni-corvinus.hu>
2021-04-22 15:05:56 +0200

    8. hét, REST API, controllers

---
+++ HajosTeszt/HajosTeszt/wwwroot/kerdesek.js
@@ -0,0 +1,44 @@
+﻿var léptetés = 0;
+
+fetch('questions/4')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+function kérdésMegjelenítés(kérdés) {
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz1").classList.add("rossz")
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz2").classList.add("rossz")
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("válasz3").classList.add("rossz")
+
+    document.getElementById("válasz" + kérdés.correctAnswer).classList.add("jó");
+    document.getElementById("válasz" + kérdés.correctAnswer).classList.remove("rossz");
+
+    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+}
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        }).then(data => kérdésMegjelenítés(data));
+}
+
+function következőKérdés() {
+    léptetés++;
+    kérdésBetöltés(léptetés);
+}
+
+function előzőKérdés() {
+    léptetés--;
+    kérdésBetöltés(léptetés);
+}
